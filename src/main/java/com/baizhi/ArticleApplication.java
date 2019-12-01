a4680208af474cc341c5839054a5329454e89740
zj <1184072059@qq.com>
2019-12-01 15:26:19 +0800

    dev

--- src/main/java/com/baizhi/ArticleApplication.java
+++ src/main/java/com/baizhi/ArticleApplication.java
@@ -11,5 +11,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ArticleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArticleApplication.class, args);
+        System.out.println("dev");
     }
 }
